20a841940aa797bf2d0a7440b1b6feb3bdcde41e
christine <cylee4@gmail.com>
2024-12-03 18:41:49 -0800

    day 3 part 2

--- day3.js
+++ day3.js
@@ -13,6 +13,28 @@ function day3() {
 	return sum;
 }
 
+function day3part2() {
+	let isMulCallValid = true;
+	let sum = 0;
+	const input = $('pre').textContent;
+	const reg = /(mul\([0-9]+\,[0-9]+\))|(don't\(\))|(do\(\))/g;
+	const matches = input.matchAll(reg);
+	for (const b of matches) {
+		const cmd = b[0];
+		if (cmd.includes("don't")) {
+			isMulCallValid = false;
+		} else if (cmd.includes("do")) {
+			isMulCallValid = true;
+		} else {
+			// is mul call
+			if (isMulCallValid) {
+				sum += eval(cmd);
+			}
+		}
+	}
+	return sum;
+}
+
 function mul(arg1, arg2) {
 	return arg1 * arg2;
 }
